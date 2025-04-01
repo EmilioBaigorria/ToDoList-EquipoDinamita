86c29b1382ed043192295fd0f5f7ca9157edaece
EmilioBaigorria <emiliobaigorriafacultad@gmail.com>
2025-04-01 06:41:27 -0300

    Añadi nuevos tipos e instale dotenv

---
+++ toDoListApp/src/http/appRequests.ts
@@ -0,0 +1,9 @@
+import { ITask } from "../types/ITask";
+
+export const putTask=async(el:ITask[])=>{
+    try {
+        
+    } catch (error) {
+        console.log("Ocurrio un erro actualizando las tareas",error)
+    }
+}
