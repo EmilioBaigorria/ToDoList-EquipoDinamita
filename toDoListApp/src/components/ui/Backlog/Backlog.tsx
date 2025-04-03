b3840f73756a1b6c74fcde14e1867c6556ae49ae
Tonga <gaston.astudillo@gmail.com>
2025-04-03 10:30:37 -0300

    modal linkeado a creartarea

--- toDoListApp/src/components/ui/Backlog/Backlog.tsx
+++ toDoListApp/src/components/ui/Backlog/Backlog.tsx
@@ -1,29 +1,37 @@
+import { useState } from "react"
+import { ModalCrearTarea } from "../../modals/ModalCrearTarea/modalCrearTarea"
 import { Button } from "../Button/Button"
 import { TaskCard } from "../TaskCard/TaskCard"
 import styles from "./Backlog.module.css"
 
 export const Backlog = () => {
 
-    const task : string[]=["tarea 1","tarea 2","tarea 3","tarea 4","tarea 5"]
+    const [activeModal, setActiveModal] = useState(false)
+
+    const task: string[] = ["tarea 1", "tarea 2", "tarea 3", "tarea 4", "tarea 5"]
     //const task : string[]=["tarea 1"]
-  return (
-    <div className={styles.mainBacklogContainer}>
-        <div>
+    return (
+        <div className={styles.mainBacklogContainer}>
             <div>
-                Backlog
-            </div>
-            <div className={styles.buttonContainer}>
                 <div>
-                    Tareas
+                    Backlog
                 </div>
-                <Button text="Crear Tarea" action={()=>{}}/>
+                <div className={styles.buttonContainer}>
+                    <div>
+                        Tareas
+                    </div>
+                    <Button text="Crear Tarea" action={()=>setActiveModal(true)} />
+                    <ModalCrearTarea
+                    isOpen={activeModal}
+                    onClose={()=>setActiveModal(false)}
+                    />
+                </div>
+            </div>
+            <div className={styles.taskListContainer}>
+                {task.map((el) => (
+                    <TaskCard data={el} />
+                ))}
             </div>
         </div>
-        <div className={styles.taskListContainer}>
-            {task.map((el)=>(
-                <TaskCard data={el}/>
-            ))}
-        </div>
-    </div>
-  )
+    )
 }
